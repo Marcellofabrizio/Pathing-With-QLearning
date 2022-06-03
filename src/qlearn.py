2004a1247754b26c9ceb35cbcf92f5d69c421d86
Marcello Fabrizio <marcello@pop-os.localdomain>
2022-06-03 12:40:57 -0300

    First

---
+++ src/qlearn.py
@@ -0,0 +1,36 @@
+import random
+import numpy as np
+
+class QLearn:
+
+    def __init__(self, states_size, actions_size, gamma = .15, eppsilon = .3):
+        self.__gamma = gamma
+        self.__epsilon = eppsilon
+        self.__q_table = np.zeros((states_size, actions_size))
+
+    def take_action(self, state):
+
+        if random.uniform(0,1) < self.__epsilon:
+            'Explore: select random action'
+
+        else:
+            'Exploit: return action with maximum score'
+
+    def explore(self, state):
+        '''
+        The agent will randomly check actions to explore its states and 
+        environment. 
+        '''
+
+    def exploit(self, state):
+        '''
+        The agent will check all possible actions from given state and
+        slects the action with the maximum value between them. 
+        '''
+
+    def update_q_table(self, state, action, new_state):
+        '''
+        Update the new_state in __q_table with a new action value
+
+        Q[state, action] = Q[state, action] + lr * (reward + gamma * np.max(Q[new_state, :]) — Q[state, action])
+        '''
