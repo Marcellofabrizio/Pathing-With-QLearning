7b3571d096676051db288129d8b484e5267184ce
Marcello Fabrizio <marcello@pop-os.localdomain>
2022-06-07 22:42:24 -0300

    Enviroment class with sample map

---
+++ src/enviroment.py
@@ -0,0 +1,10 @@
+class Environment:
+
+    def __init__(self, map):
+        self.rows = len(map)
+        self.cols = len(map[0])
+        self.map = map
+
+    @property
+    def shape(self):
+        return (self.rows, self.cols)
