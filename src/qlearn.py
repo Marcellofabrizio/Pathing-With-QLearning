3fcaade4b3b63e623abf0d1d47e1b3dee0880ffc
Marcello Fabrizio <39314230+Marcellofabrizio@users.noreply.github.com>
2022-06-09 22:51:06 -0300

    Mudanças feitas no final da aula

--- src/qlearn.py
+++ src/qlearn.py
@@ -4,20 +4,20 @@ import numpy as np
 
 class QLearn:
 
-    def __init__(self, environment, gamma=.80, eppsilon=.3):
+    def __init__(self, enviroment, gamma=.80, eppsilon=.3):
         self.__actions = ['up', 'right', 'down', 'left']
         # gamma will be used to balance immediate and future reward
         self.__gamma = gamma
-        # eppsilon is the rate in which exploration will be performed over exploitation
+        # eppsilon the rate in which exploration will be performed over exploitation
         self.__eppsilon = eppsilon
 
-        self.__env_rows, self.__env_cols = environment.shape
+        self.__env_rows, self.__env_cols = enviroment.shape
 
         # three dimensional array for States and Actions
         self.__q_table = np.zeros((self.__env_rows, self.__env_cols, 4))
 
         # two dimensional array for rewards per State
-        self.__rewards = environment.map
+        self.__rewards = enviroment.map
 
     def train(self, episodes):
         '''
@@ -27,11 +27,13 @@ class QLearn:
 
         for i in range(episodes):
 
-            row, col = 3, 5
+            row, col = 2, 2
 
-            while self.__rewards[row, col] == -1:
+            while self.__rewards[row][col] == -1:
 
                 row, col = self.update_q_value(row, col)
+                if(self.__rewards[row][col] == 100):
+                    print(row, col)
 
 
     def update_q_value(self, row, col):
@@ -55,6 +57,7 @@ class QLearn:
         temp_difference = reward + self.__gamma * np.max(self.__q_table[row, col]) - old_q_value
 
         self.__q_table[row, col, action] = old_q_value + temp_difference
+        # print(self.__q_table[row, col, action])
         return row, col
 
     def get_initial_state(self):
@@ -65,10 +68,10 @@ class QLearn:
     def reward(self, row, col):
         '''
         Returns the given reward for state, action.
-        In our case, this will be the value from the environment
+        In our case, this will be the value from the enviroment
         '''
 
-        return self.__rewards[row, col]
+        return self.__rewards[row][col]
 
     def get_next_location(self, env_row, env_col, action):
 
@@ -91,21 +94,40 @@ class QLearn:
 
         if random.uniform(0, 1) < self.__eppsilon:
             'Explore: select random action'
-            return self.explore(env_row, env_col)
+            return self.explore()
         else:
             'Exploit: return action with maximum score'
-            return self.exploit()
+            return self.exploit(env_row, env_col)
 
     def explore(self):
         '''
         The agent will randomly check actions to explore its states and 
         environment. 
         '''
-        return random.randint(4)
+        return random.randint(0,3)
 
     def exploit(self, env_row, env_col):
         '''
         The agent will check all possible actions from given state and
-        selects the action with the maximum value between them. 
+        slects the action with the maximum value between them. 
         '''
         return np.argmax(self.__q_table[env_row, env_col])
+
+    def get_shortest_path(self, initial_row, initial_col):
+
+        shortest_path = []
+
+        current_row = initial_row
+        current_col = initial_col 
+
+        if self.__rewards[initial_row][initial_col] == -100:
+            return []
+
+        else:
+
+            while self.__rewards[current_row][current_col] == -1:
+                action = self.exploit(current_row, current_col)
+                current_row, current_col = self.get_next_location(current_row, current_col, action)
+                shortest_path.append((current_row, current_col))
+
+        return shortest_path
