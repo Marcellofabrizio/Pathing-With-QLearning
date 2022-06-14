0d657bed10b535242f6c80864f543cd3f3aee2f7
Kbzinho-66 <gvalles@ucs.br>
2022-06-13 22:01:19 -0300

    Definição dos parâmetros por argumento

--- src/data.py
+++ src/data.py
@@ -1,6 +1,24 @@
-WALL = -100
+from codes import Rewards
 
-final_environment = [
+WALL = Rewards.WALL
+
+# Not using the codes themselves here makes it easier to visualize it
+test_map = [
+    # 0    #1    #2    #3    #4    #5    #6    #7    #8    #9    #10
+    [WALL, WALL, WALL, WALL, WALL,  100, WALL, WALL, WALL, WALL, WALL],  # 0
+    [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL],  # 1
+    [WALL,   -1, WALL, WALL, WALL, WALL, WALL,   -1, WALL,   -1, WALL],  # 2
+    [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL,   -1, WALL],  # 3
+    [WALL, WALL, WALL,   -1, WALL, WALL, WALL,   -1, WALL, WALL, WALL],  # 4
+    [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1],  # 5
+    [WALL, WALL, WALL, WALL, WALL,   -1, WALL, WALL, WALL, WALL, WALL],  # 6
+    [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL],  # 7
+    [WALL, WALL, WALL,   -1, WALL, WALL, WALL,   -1, WALL, WALL, WALL],  # 8
+    [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1],  # 9
+    [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL]  # 10
+]
+
+validation_map = [
     [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL],
     [WALL,   -1,   -1,   -1,   -1, WALL,   -1,   -1,   -1,   -1,   -1,   -1, WALL, WALL],
     [WALL,   -1, WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL, WALL],
--- src/main.py
+++ src/main.py
@@ -1,48 +1,44 @@
 from environment import Environment
 from qlearn import QLearn
-from codes import Rewards
+import argparse
+import data
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Finding the optimal path to an objective with Q-Learning.')
+
+    parser.add_argument('-f', '--fixed-exploration',
+                        action='store_true', dest='fixed_exploration',
+                        help='Forces every episode to begin at the same position.')
+
+    parser.add_argument('episodes', type=int,
+                        help='Number of episodes used for training.')
+
+    args = parser.parse_args()
+    return args.fixed_exploration, args.episodes
+
 
 if __name__ == '__main__':
-    WALL = Rewards.WALL
 
-    # Not using the codes themselves here makes it easier to visualize it
-    sample_environment = [
-         # 0    #1    #2    #3    #4    #5    #6    #7    #8    #9    #10
-        [WALL, WALL, WALL, WALL, WALL,  100, WALL, WALL, WALL, WALL, WALL],  # 0
-        [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL],  # 1
-        [WALL,   -1, WALL, WALL, WALL, WALL, WALL,   -1, WALL,   -1, WALL],  # 2
-        [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL,   -1, WALL],  # 3
-        [WALL, WALL, WALL,   -1, WALL, WALL, WALL,   -1, WALL, WALL, WALL],  # 4
-        [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1],  # 5
-        [WALL, WALL, WALL, WALL, WALL,   -1, WALL, WALL, WALL, WALL, WALL],  # 6
-        [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL],  # 7
-        [WALL, WALL, WALL,   -1, WALL, WALL, WALL,   -1, WALL, WALL, WALL],  # 8
-        [  -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1],  # 9
-        [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL]   # 10
-    ]
+    fixed_exploration, episodes = parse_args()
+    r = parse_args()
 
-    final_environment = [
-         # 0    #1    #2    #3    #4    #5    #6    #7    #8    #9   #10   #11   #12   #13
-        [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL],  # 0
-        [WALL,   -1,   -1,   -1,   -1, WALL,   -1,   -1,   -1,   -1,   -1,   -1, WALL, WALL],  # 1
-        [WALL,   -1, WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1, WALL, WALL],  # 2
-        [WALL, WALL,   -1, WALL,   -1,   -1,   -1, WALL,   -1, WALL, WALL,   -1,   -1, WALL],  # 3
-        [WALL,   -1, WALL,   -1,   -1,   -1,   -1,   -1,   -1, WALL,   -1,   -1,   -1, WALL],  # 4
-        [WALL,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,  100, WALL],  # 5
-        [WALL, WALL, WALL, WALL, WALL,   -1,   -1, WALL,   -1, WALL, WALL, WALL, WALL, WALL],  # 6
-        [WALL, WALL, WALL, WALL, WALL,   -1,   -1,   -1,   -1, WALL, WALL, WALL, WALL, WALL],  # 7
-        [WALL, WALL, WALL, WALL, WALL,   -1,   -1,   -1,   -1, WALL, WALL, WALL, WALL, WALL],  # 8
-        [WALL, WALL, WALL, WALL, WALL,   -1,   -1, WALL,   -1, WALL, WALL, WALL, WALL, WALL],  # 9
-        [WALL, WALL, WALL, WALL, WALL,   -1,   -1,   -1,   -1, WALL, WALL, WALL, WALL, WALL],  # 10
-        [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL]   # 11
-    ]
+    if fixed_exploration:
+        print('Not yet implemented.')
 
-    env = Environment(final_environment)
+    else:
+        env = Environment(data.validation_map)
+        qlearn = QLearn(env)
+        qlearn.train(episodes)
 
-    qlearn = QLearn(env)
+        while True:
+            position = input('Please insert the starting position or press Enter to exit: ')
+            try:
+                initial_row, initial_col = [int(n) for n in position.split(' ')]
+            except ValueError:
+                if position:
+                    print('Invalid position.')
+                break
 
-    qlearn.train(1000)
-
-    # qlearn.print_qtable()
-    shortest_path = qlearn.get_shortest_path(initial_row=10, initial_col=5)
-    qlearn.print_shortest_path(shortest_path, initial_row=10, initial_col=5)
+            shortest_path = qlearn.get_shortest_path(initial_row, initial_col)
+            qlearn.print_shortest_path(shortest_path, initial_row, initial_col)
